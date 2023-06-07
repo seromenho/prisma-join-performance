01e055bdbd114c97ac533dbfaab28f3dafa92313
Ricardo Seromenho <ricardo.seromenho@gmail.com>
2023-06-07 10:48:04 +0100

    Update prisma version to 4.15.0

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
-  testEnvironment: 'node',
-  moduleFileExtensions: [ 'ts', 'tsx', 'js', 'jsx', 'json', 'node', 'html' ]
-}
+  preset: "ts-jest/presets/js-with-ts",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node", "html"],
+  testTimeout: 30000,
+};
